4f23263b6257a0ea149352c26c091267dfc26465
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-10-05 12:16:05 -0600

    added in some logic to parse backup cfs data

--- index.js
+++ index.js
@@ -19,70 +19,75 @@ const scrapeNps = async () => {
   const page = await browser.newPage();
 
   try {
-    
-  
-  await page.goto("https://auth.podium.com", { waitUntil: "networkidle2" });
-  await page.evaluate(() => {
-    document.getElementById("emailOrPhoneInput").click();
-  });
-  await page.keyboard.type(process.env.P_EMAIL);
-  await page.evaluate(() => {
-    document.getElementById("signInButton").click();
-  });
-  await page.waitFor(2000);
-  await page.evaluate(() => {
-    document.getElementById("passwordInput");
-  });
-  await page.keyboard.type(process.env.P_PASS);
-  await page.evaluate(() => {
-    document.getElementById("signInButton").click();
-  });
+    await page.goto("https://auth.podium.com", { waitUntil: "networkidle2" });
+    await page.evaluate(() => {
+      document.getElementById("emailOrPhoneInput").click();
+    });
+    await page.keyboard.type(process.env.P_EMAIL);
+    await page.evaluate(() => {
+      document.getElementById("signInButton").click();
+    });
+    await page.waitFor(2000);
+    await page.evaluate(() => {
+      document.getElementById("passwordInput");
+    });
+    await page.keyboard.type(process.env.P_PASS);
+    await page.evaluate(() => {
+      document.getElementById("signInButton").click();
+    });
 
-  await page.waitForSelector("#selenium-insights");
+    await page.waitForSelector("#selenium-insights");
 
-  await page.goto(process.env.P_SECRET_URL, { waitUntil: "networkidle2" });
-  await page.waitFor(10000);
+    await page.goto(process.env.P_SECRET_URL, { waitUntil: "networkidle2" });
+    await page.waitFor(10000);
 
-  await page.goto(process.env.P_D_URL, { waitUntil: "networkidle2" });
+    await page.goto(process.env.P_D_URL, { waitUntil: "networkidle2" });
 
-  await page.waitForSelector("i.icon-wrench");
-  await page.waitFor(3000)
-  await page.evaluate(async () => {
+    await page.waitForSelector("i.icon-wrench");
+    await page.waitFor(3000);
+    await page.evaluate(async () => {
+      await document.querySelector("i.icon-wrench").click();
 
-    await document.querySelector("i.icon-wrench").click();
-
-    await document.querySelectorAll("div.db-text-body.label")[1].click();
-    await document.getElementById("export-csv").click();
-  });
-  await page.waitFor(3000);
-  await browser.close();
-  console.log("browser shut down");
+      await document.querySelectorAll("div.db-text-body.label")[1].click();
+      await document.getElementById("export-csv").click();
+    });
+    await page.waitFor(3000);
+    await browser.close();
+    console.log("browser shut down");
   } catch (error) {
-    console.log(error)
-}
+    console.log(error);
+  }
 };
 
 const addNps = async (db, seed) => {
   //change me
 
-  await db.query('delete from podium_nps')
-  let start = performance.now();
+  await db.query("delete from podium_nps");
 
 
-  const csvFilePath = path.resolve(
-    __dirname,
-    `NPS.csv`
-  );
+  const csvFilePath = path.resolve(__dirname, `NPS.csv`);
 
-  console.log(csvFilePath, 'faweoifj')
+  console.log(csvFilePath, "faweoifj");
   const jsonArray = await csv().fromFile(csvFilePath);
+  console.log(jsonArray.length);
+  
   let filtered;
   let promises = [];
-  if (!seed) {
+  if (!seed && jsonArray[0]["Response Date"]) {
     filtered = jsonArray.filter(el => {
       return (
-        moment(el["Response Date"]).format("YYYY-MM-DD")>=
-        moment().startOf('month')
+        moment(el["Response Date"]).format("YYYY-MM-DD") >=
+        moment()
+          .startOf("month")
+          .format("YYYY-MM-DD")
+      );
+    });
+  } else if (!seed && jsonArray[0]["Response Received At"]) {
+    filtered = jsonArray.filter(el => {
+      return (
+        moment(el["Response Received At"]).format("YYYY-MM-DD") >=
+        moment()
+          .startOf("month")
           .format("YYYY-MM-DD")
       );
     });
@@ -91,55 +96,75 @@ const addNps = async (db, seed) => {
   }
   console.log(filtered.length, "array length");
   for (let i = 0; i < filtered.length; i++) {
-    console.log(i, filtered.length, 'prog')
+    console.log(i, filtered.length, "prog");
     let base = filtered[i];
     //db adder
     console.log("progress", i, "/", filtered.length);
     //add in logic to check the add date. only add from yesterday?
-    let identifier = await JSON.stringify({
-      invite: base["Invite Date"],
-      res: base["Response Date"],
-      phone: base["Phone"],
-      loc: base["Location"],
-      tech: base["Technician"],
-      customer: base["Customer Name"],
-      rating: +base["Rating"],
-      comment: base["Comment"]
-    });
 
-    try {
-       db.add_nps([
-        base["Invite Date"],
-        base["Response Date"],
-        base["Phone"],
-        base["Location"],
-        base["Technician"],
-        base["Customer Name"],
-        +base["Rating"],
-        base["Comment"],
-        identifier
-      ]);
-      // promises.push(
-      //   db.add_nps([
-      //     base["Invite Date"],
-      //     base["Response Date"],
-      //     base["Phone"],
-      //     base["Location"],
-      //     base["Technician"],
-      //     base["Customer Name"],
-      //     +base["Rating"],
-      //     base["Comment"],
-      //     identifier
-      //   ])
-      // );
-    } catch (error) {
-      console.log(error, "error with podium adder");
+    if (base["Invite Date"]) {
+      console.log('yEET')
+      let identifier = await JSON.stringify({
+        invite: base["Invite Date"],
+        res: base["Response Date"],
+        phone: base["Phone"],
+        loc: base["Location"],
+        tech: base["Technician"],
+        customer: base["Customer Name"],
+        rating: +base["Rating"],
+        comment: base["Comment"]
+      });
+
+      try {
+        db.add_nps([
+          base["Invite Date"],
+          base["Response Date"],
+          base["Phone"],
+          base["Location"],
+          base["Technician"],
+          base["Customer Name"],
+          +base["Rating"],
+          base["Comment"],
+          identifier
+        ]);
+      } catch (error) {
+        console.log(error, "error with podium adder");
+      }
+    }
+
+    if (base["Response Received At"]) {
+
+      let identifier = await JSON.stringify({
+        invite: base["Invite Date"],
+        res: base["Response Date"],
+        phone: base["Phone"],
+        loc: base["Location"],
+        tech: base["Attributions"],
+        customer: base["Customer Name"],
+        rating: +base["Rating"],
+        comment: base["Comment"]
+      });
+      try {
+        db.add_nps([
+          moment(base["Survey Sent At"]).format("YYYY-MM-DD"),
+          moment(base["Response Received At"]).format("YYYY-MM-DD"),
+          base["Phone Number"],
+          base["Location Name"],
+          base["Attributions"],
+          base["Customer Name"],
+          +base["Score"],
+          base["Message"],
+          identifier
+        ]);
+      } catch (error) {
+        console.log(error, "error with backup adder");
+      }
     }
 
     //delete downloaded file
   }
   Promise.all(promises).then(() => {
-    console.log("finished adding podium data", performance.now() - start);
+    console.log("finished adding podium data");
   });
 };
 
@@ -148,25 +173,17 @@ const attNps = async db => {
     __dirname,
     `/../../../Downloads/NPS+Export+(Based+on+Service+Date).csv`
   );
-  
-
 
   let employees = await db.query(
-    
-    'select distinct employee_id, e_f_name, e_last_name  from paylocity_hours ph'
+    "select distinct employee_id, e_f_name, e_last_name  from paylocity_hours ph"
   );
 
   console.log(employees.length, "emp length");
   // and is_active=1
   let promises = [];
-//fix this to run off of paylocity id instead of pestroutes ID. 
-
+  //fix this to run off of paylocity id instead of pestroutes ID.
 
   for (let i = 0; i < employees.length; i++) {
-
-
-    
-
     let likeStr = `${employees[i].e_f_name.replace(
       employees[i].e_f_name.charAt(0),
       "%_"
@@ -175,20 +192,19 @@ const attNps = async db => {
       "_"
     )}%`;
 
-    likeStr=likeStr.replace(/ /gi, '%')
+    likeStr = likeStr.replace(/ /gi, "%");
     // if(+employees[i].employee_id==102254){
 
-      console.log(likeStr, i, employees.length);
+    console.log(likeStr, i, employees.length);
     // }
 
     // promises.push(db.att_podium_nps([employees[i].proutes_id, likeStr]));
 
-     db.att_podium_nps([employees[i].employee_id, likeStr]);
-  
+    db.att_podium_nps([employees[i].employee_id, likeStr]);
   }
-  
+
   // fs.unlinkSync(delPath);
-  console.log('file deleted')
+  console.log("file deleted");
 
   // Promise.all(promises).then(() => {
   //   let finish = performance.now();
@@ -204,11 +220,10 @@ const attNps = async db => {
 
 const fireAll = async () => {
   let database = await massive({
-    
     user: process.env.G_DB_USER,
     password: process.env.G_DB_PASS,
     database: process.env.G_DB_NAME,
-    host: process.env.G_DB_HOST,
+    host: process.env.G_DB_HOST
     //DO
     // connectionString: DB_STRING,
     // ssl: true
